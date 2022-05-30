5545944ce39d1b66835bc439d1c3993611c0a604
Prachi Suthar <prachi_suthar@mymail.sutd.edu.sg>
2022-05-30 12:44:18 +0800

    Update models.py

--- escproject/escapp/models.py
+++ escproject/escapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+# from .forms import ModelForm 
 # Create your models here.
+
+
+COUNTRY_CHOICES = (
+    ('Singapore','Singapore'),
+    ('London', 'London'),
+    ('Malaysia','Malaysia'),
+    )
+
+class DestinationSearch(models.Model):
+    country = models.CharField(max_length=250, choices = COUNTRY_CHOICES, default = 'Singapore')
+    pax = models.IntegerField()
+
+
+    def __str__(self):
+        return self.country()
+
