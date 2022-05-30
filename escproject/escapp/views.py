e6f74b7fe14962ecdbbc7c21a452d1636eadcc44
Prachi Suthar <prachi_suthar@mymail.sutd.edu.sg>
2022-05-30 12:44:22 +0800

    Update views.py

--- escproject/escapp/views.py
+++ escproject/escapp/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, FormView, CreateView
+from .forms import *
+from django.urls import reverse_lazy
+from .models import *
 
-class HomeView(TemplateView):
+
+#create views here 
+class HomeView(CreateView):
     template_name = "home.html"
+    form_class = DestinationSearchForm
+    success_url = reverse_lazy("escapp:home")
 
+    def form_valid(self, form):
+        return super().form_valid(form)
 
-# Create your views here.
