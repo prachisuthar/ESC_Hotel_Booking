868a4e26a7b475913f12a66dcfed1043040f7342
Prachi Suthar <prachi_suthar@mymail.sutd.edu.sg>
2022-05-30 13:22:56 +0800

    Update forms.py

--- escproject/escapp/forms.py
+++ escproject/escapp/forms.py
@@ -1,10 +1,19 @@
 from dataclasses import fields
+from tkinter.ttk import Widget
 from django import forms
 from .models import *
 from .views import * 
+from django.forms import ModelForm
+
+class DateInput(forms.DateInput):
+    input_type = 'date'
 
 
 class DestinationSearchForm(forms.ModelForm):
     class Meta:
         model = DestinationSearch
-        fields = ["country","pax",]
+        fields = ["country","pax","start_date", "end_date"]
+        widgets = {
+            'start_date' : DateInput(),
+            'end_date' : DateInput()
+        }
